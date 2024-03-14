a9fdd5592bd686fc927130a9a5fda81acee4dee3
Lars-Christian Selland HVL <141957545+h678145@users.noreply.github.com>
2024-03-14 17:16:49 +0100

    Lagt til schema samt lagt til testdata

--- prosjekt.sql
+++ prosjekt.sql
@@ -1,3 +1,7 @@
+DROP SCHEMA IF EXISTS Dat107Oblig3 CASCADE;
+CREATE SCHEMA Dat107Oblig3;
+SET search_path TO Dat107Oblig3;
+
 create Table Ansatt
 (
     ansatt_id       SERIAL PRIMARY KEY,
@@ -6,9 +10,17 @@ create Table Ansatt
     etternavn       VARCHAR(255)      NOT NULL,
     ansettelsesdato DATE              NOT NULL,
     stilling        VARCHAR(255)      NOT NULL,
-    manedslonn      NUMERIC(10, 2)    NOT NULL
-    /*avdeling_id     INT               NOT NULL*/
+    manedslonn      NUMERIC(10, 2)    NOT NULL 
 );
+INSERT INTO
+    Ansatt(brukernavn,fornavn,etternavn,ansettelsesdato,stilling,manedslonn)
+    VALUES
+    ('evs','Edvard','Steenslid',2012-08-24,'Programmeringsmyrder',4206969.69
+    );
+SELECT * FROM Ansatt;
+
+
+ /*avdeling_id     INT               NOT NULL*/
 /*CREATE TABLE Avdeling
 (
     avdeling_id   SERIAL PRIMARY KEY,
