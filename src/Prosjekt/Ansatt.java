cf12a61103c2633eae1ea41e46c8c8ecd31b6cc5
h581296 <581296@stud.hvl.no>
2024-03-15 12:23:42 +0100

    Update Ansatt.java

--- src/Prosjekt/Ansatt.java
+++ src/Prosjekt/Ansatt.java
@@ -1,8 +1,14 @@
 package Prosjekt;
 
+import jakarta.persistence.Entity;
+
 @Entity
 public class Ansatt {
 
+    @Id
+    private int ansattID;
+    private String brukernavn, fornavn, etternavn;
+    private double manedslonn;
 
 
 }
