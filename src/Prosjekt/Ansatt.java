7571a89bb562fac8bbc26f7125e6a7010b7920d4
h581296 <581296@stud.hvl.no>
2024-03-15 12:38:29 +0100

    a

--- src/Prosjekt/Ansatt.java
+++ src/Prosjekt/Ansatt.java
@@ -2,6 +2,8 @@ package Prosjekt;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class Ansatt {
 
@@ -12,49 +14,8 @@ public class Ansatt {
     @Column(columnDefinition = "TEXT")
     private String brukernavn, fornavn, etternavn, stilling;
 
+    @Temporal(TemporalType.DATE)
+    private Date ansettelsesDato; //?
     private double manedslonn;
-
-    public int getAnsattID() {
-        return ansattID;
-    }
-
-    public String getBrukernavn() {
-        return brukernavn;
-    }
-
-    public void setBrukernavn(String brukernavn) {
-        this.brukernavn = brukernavn;
-    }
-
-    public String getFornavn() {
-        return fornavn;
-    }
-
-    public void setFornavn(String fornavn) {
-        this.fornavn = fornavn;
-    }
-
-    public String getEtternavn() {
-        return etternavn;
-    }
-
-    public void setEtternavn(String etternavn) {
-        this.etternavn = etternavn;
-    }
-
-    public String getStilling() {
-        return stilling;
-    }
-
-    public void setStilling(String stilling) {
-        this.stilling = stilling;
-    }
-
-    public double getManedslonn() {
-        return manedslonn;
-    }
-
-    public void setManedslonn(double manedslonn) {
-        this.manedslonn = manedslonn;
-    }
+    
 }
