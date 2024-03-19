a8bf12a4ca79bb358225a563e49fe4b1f649aa4e
h676669 <676669@stud.hvl.no>
2024-03-19 10:51:19 +0100

    Update Avdeling.java

--- src/Prosjekt/Avdeling.java
+++ src/Prosjekt/Avdeling.java
@@ -11,4 +11,10 @@ public class Avdeling {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int avdelingID;
+
+    @Column(columnDefinition = "TEXT")
+    private String avdelingsnavn;
+
+    @Column(columnDefinition = "INT")
+    private int le_boss_id;
 }
