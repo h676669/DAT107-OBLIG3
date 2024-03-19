504de2d078be9cf7dc8b693cea28e233daabbbd2
676639 <676639@stud.hvl.no>
2024-03-19 11:03:59 +0100

    main

--- src/Prosjekt/Main.java
+++ src/Prosjekt/Main.java
@@ -1,7 +1,23 @@
 package Prosjekt;
 
 public class Main {
+    public static void main(String[] args) {
 
+        AnsattDAO ansattDAO = new AnsattDAO();
 
+        try {
+            int ansattId = 1;
+            Ansatt ansatt = ansattDAO.finnAnsattMedId(ansattId);
 
+            if (ansatt != null) {
+                System.out.println("Found ansatt: " + ansatt.getFornavn() + " " + ansatt.getEtternavn());
+            } else {
+                System.out.println("Ansatt with ID " + ansattId + " not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ansattDAO.close();
+        }
+    }
 }
