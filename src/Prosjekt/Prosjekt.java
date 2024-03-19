e1b0035a1e757b7ea4f93e26fec1d547682ef347
h676669 <676669@stud.hvl.no>
2024-03-19 10:56:09 +0100

    endre var navn

--- src/Prosjekt/Prosjekt.java
+++ src/Prosjekt/Prosjekt.java
@@ -7,10 +7,10 @@ import jakarta.persistence.*;
 public class Prosjekt {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int prosjektID;
+    private int p_id;
 
     @Column(columnDefinition = "TEXT")
-    private String navn, beskrivelse;
+    private String p_navn, p_beskrivelse;
 
     //her skal det være for ting for ansatte i prosjektet
 }
