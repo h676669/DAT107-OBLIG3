a4a22b6528af72d8546bcc5425bb34af97949e3c
676639 <676639@stud.hvl.no>
2024-03-21 12:29:03 +0100

    leggTilNyAnsatt

--- src/Prosjekt/AnsattDAO.java
+++ src/Prosjekt/AnsattDAO.java
@@ -2,6 +2,7 @@ package Prosjekt;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class AnsattDAO {
@@ -26,4 +27,21 @@ public class AnsattDAO {
             emf.close();
         }
     }
+
+    public void leggTilNyAnsatt(Ansatt nyAnsatt) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(nyAnsatt);
+            tx.commit();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
 }
