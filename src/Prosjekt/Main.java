c45628a724acb5460cde2525e3b494db6b363559
h676669 <142335301+h676669@users.noreply.github.com>
2024-03-21 12:59:04 +0100

    Update Main.java

--- src/Prosjekt/Main.java
+++ src/Prosjekt/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         AnsattDAO ansattDAO = new AnsattDAO();
+        AvdelingDAO avdelingDAO = new AvdelingDAO();
+
 
         try {
             int ansattId = 1;
@@ -13,6 +15,20 @@ public class Main {
                 System.out.println("Found ansatt: " + ansatt.getFornavn() + " " + ansatt.getEtternavn());
             } else {
                 System.out.println("Ansatt with ID " + ansattId + " not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ansattDAO.close();
+        }
+        try {
+            int avdelingID = 1;
+            Avdeling avdeling= avdelingDAO.finnAvdelingMedId(avdelingID);
+
+            if (avdeling != null) {
+                System.out.println("Found avdeling: ");
+            } else {
+                System.out.println("Avdeling with ID " + avdelingID + " not found.");
             }
         } catch (Exception e) {
             e.printStackTrace();
