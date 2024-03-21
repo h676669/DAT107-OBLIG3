856c1422f692ca24aaa51a7fbb2195adaab6a396
h581296 <581296@stud.hvl.no>
2024-03-21 14:35:32 +0100

    0

--- src/Prosjekt/Main.java
+++ src/Prosjekt/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        meny();
         AnsattDAO ansattDAO = new AnsattDAO();
         AvdelingDAO avdelingDAO = new AvdelingDAO();
 
