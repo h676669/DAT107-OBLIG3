8c69e59da187491372990b2998ad854296034d5c
h581296 <581296@stud.hvl.no>
2024-03-21 12:20:18 +0100

    getter/setter

--- src/Prosjekt/Avdeling.java
+++ src/Prosjekt/Avdeling.java
@@ -15,6 +15,42 @@ public class Avdeling {
     @Column(columnDefinition = "TEXT")
     private String avdelingsnavn;
 
+    @Column(columnDefinition = "TEXT")
+    private int avdelingAnsatt;
+
     @Column(columnDefinition = "INT")
     private int le_boss_id;
+
+
+    public int getAvdelingID() {
+        return avdelingID;
+    }
+
+    public void setAvdelingID(int avdelingID) {
+        this.avdelingID = avdelingID;
+    }
+
+    public String getAvdelingsnavn() {
+        return avdelingsnavn;
+    }
+
+    public void setAvdelingsnavn(String avdelingsnavn) {
+        this.avdelingsnavn = avdelingsnavn;
+    }
+
+    public int getAvdelingAnsatt() {
+        return avdelingAnsatt;
+    }
+
+    public void setAvdelingAnsatt(int avdelingAnsatt) {
+        this.avdelingAnsatt = avdelingAnsatt;
+    }
+
+    public int getLe_boss_id() {
+        return le_boss_id;
+    }
+
+    public void setLe_boss_id(int le_boss_id) {
+        this.le_boss_id = le_boss_id;
+    }
 }
