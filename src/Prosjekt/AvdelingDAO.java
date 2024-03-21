74d2b02fd852fa35c5f0ec1f848b5758c929efa0
676639 <676639@stud.hvl.no>
2024-03-21 12:17:16 +0100

    Create AvdelingDAO.java

---
+++ src/Prosjekt/AvdelingDAO.java
@@ -0,0 +1,10 @@
+package Prosjekt;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AvdelingDAO {
+
+    
+}
