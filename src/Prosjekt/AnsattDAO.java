c3d71a852652ca4e22dab6bb9838d75e50130271
h676669 <142335301+h676669@users.noreply.github.com>
2024-03-21 12:34:40 +0100

    legge til ansatt i tabell

--- src/Prosjekt/AnsattDAO.java
+++ src/Prosjekt/AnsattDAO.java
@@ -1,9 +1,16 @@
 package Prosjekt;
 
+<<<<<<< Updated upstream
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+=======
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+>>>>>>> Stashed changes
 
 public class AnsattDAO {
 
@@ -21,6 +28,71 @@ public class AnsattDAO {
             em.close();
         }
     }
+<<<<<<< Updated upstream
+=======
+    public Ansatt finnAnsattMedBrukernanv(String brukernavn) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<Ansatt> query = em.createQuery("select t from Ansatt t where t.brukernavn like :brukernavn", Ansatt.class);
+            query.setParameter("brukernavn",brukernavn);
+            return query.getSingleResult(); // NB! Exception hvis ingen eller flere resultater
+        } finally {
+            em.close();
+        }
+    }
+    public List<Ansatt> finnAlleAnsatt(){
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            TypedQuery<Ansatt> query = em.createQuery("select t from Ansatt t", Ansatt.class);
+            return query.getResultList();
+        }
+        finally {
+            em.close();
+        }
+    }
+    public void oppdaterAnsatt(int id, String stilling, double lonn){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Ansatt managedAnsatt = finnAnsattMedId(id);
+            managedAnsatt.setManedslonn(lonn);
+            managedAnsatt.setStilling(stilling);
+            tx.commit();
+        }
+        catch (Throwable e){
+            e.printStackTrace();
+            if(tx.isActive()){
+                tx.rollback();
+            }
+        }
+        finally {
+            em.close();
+        }
+    }
+    public void lagreNyAnsatt(Prosjekt prosjekt, String brukernavn, String fornavn,
+                              String etternavn, String stilling, LocalDate ansettelsesDato,
+                              Double manedslonn){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Ansatt nyAnsatt = new Ansatt(prosjekt,brukernavn,fornavn,etternavn,stilling,ansettelsesDato,manedslonn);
+            em.persist(nyAnsatt);
+            tx.commit();
+        }
+        catch (Throwable e){
+            e.printStackTrace();
+            if(tx.isActive()){
+                tx.rollback();
+            }
+        }
+        finally {
+        em.close();
+        }
+    }
+>>>>>>> Stashed changes
 
     public void close() {
         if (emf != null) {
