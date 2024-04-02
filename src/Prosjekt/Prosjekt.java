58235e6b84c953e4a0c87bb1fad6e18b4312bf94
h676669 <142335301+h676669@users.noreply.github.com>
2024-04-02 11:30:28 +0200

    Update Prosjekt.java

--- src/Prosjekt/Prosjekt.java
+++ src/Prosjekt/Prosjekt.java
@@ -15,10 +15,48 @@ public class Prosjekt {
     private String prosjekt_navn, prosjekt_beskrivelse;
 
     @OneToMany(mappedBy = "prosjekt") // Corrected mapping field name
-    private List<ProsjektDeltagelse> ProsjektListe;
+    private List<ProsjektDeltagelse> prosjektListe;
+
+    public int getProsjekt_id() {
+        return prosjekt_id;
+    }
+
+    public void setProsjekt_id(int prosjekt_id) {
+        this.prosjekt_id = prosjekt_id;
+    }
+
+    public String getProsjekt_navn() {
+        return prosjekt_navn;
+    }
+
+    public void setProsjekt_navn(String prosjekt_navn) {
+        this.prosjekt_navn = prosjekt_navn;
+    }
+
+    public String getProsjekt_beskrivelse() {
+        return prosjekt_beskrivelse;
+    }
+
+    public void setProsjekt_beskrivelse(String prosjekt_beskrivelse) {
+        this.prosjekt_beskrivelse = prosjekt_beskrivelse;
+    }
+
+    public List<ProsjektDeltagelse> getProsjektListe() {
+        return prosjektListe;
+    }
+
+    public void setProsjektListe(List<ProsjektDeltagelse> prosjektListe) {
+        this.prosjektListe = prosjektListe;
+    }
 
     public Prosjekt() {
 
+    }
+    public Prosjekt(int prosjekt_id,String prosjekt_beskrivelse, String prosjekt_navn, List<ProsjektDeltagelse> prosjektListe){
+        this.prosjekt_id = prosjekt_id;
+        this.prosjekt_beskrivelse = prosjekt_beskrivelse;
+        this.prosjekt_navn = prosjekt_navn;
+        this.prosjektListe = prosjektListe;
     }
 
 }
