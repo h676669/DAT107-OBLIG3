dffab94a8ff07779a7dc5360ae761c5a4dc63ce1
Lars-Christian Selland HVL <141957545+h678145@users.noreply.github.com>
2024-04-02 12:24:16 +0200

    Update ProsjektDAO.java

--- src/Prosjekt/ProsjektDAO.java
+++ src/Prosjekt/ProsjektDAO.java
@@ -1,4 +1,24 @@
 package Prosjekt;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class ProsjektDAO {
+
+
+    private EntityManagerFactory emf;
+
+    public ProsjektDAO() {
+        emf = Persistence.createEntityManagerFactory("Prosjekt");
+    }
+    public Prosjekt finnProsjektMedId(int id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Prosjekt.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
 }
