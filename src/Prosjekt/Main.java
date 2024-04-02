3fcfe24468a0be1df2dd1577cc35e524b5aca310
h581296 <581296@stud.hvl.no>
2024-04-02 10:37:37 +0200

    Update Main.java

--- src/Prosjekt/Main.java
+++ src/Prosjekt/Main.java
@@ -6,23 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        AnsattDAO ansattDAO = new AnsattDAO();
+        meny();
+
+        /*AnsattDAO ansattDAO = new AnsattDAO();
         AvdelingDAO avdelingDAO = new AvdelingDAO();
 
-        try {
-            int ansattId = 1;
-            Ansatt ansatt = ansattDAO.finnAnsattMedId(ansattId);
 
-            if (ansatt != null) {
-                System.out.println("Found ansatt: " + ansatt.getFornavn() + " " + ansatt.getEtternavn());
-            } else {
-                System.out.println("Ansatt with ID " + ansattId + " not found.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            ansattDAO.close();
-        }
         try {
             int avdelingID = 1;
             Avdeling avdeling= avdelingDAO.finnAvdelingMedId(avdelingID);
@@ -46,12 +35,15 @@ public class Main {
             e.printStackTrace();
         } finally {
             avdelingDAO.close();
-        }
+        }*/
     }
 
 
     //TODO alt
     public static void meny() {
+        AnsattDAO ansattDAO = new AnsattDAO();
+        AvdelingDAO avdelingDAO = new AvdelingDAO();
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Trykk ein tast på hustelefonen for å velje eit valg");
         System.out.println("1: Søk etter ansatt på ansatt-ID");
@@ -76,7 +68,7 @@ public class Main {
         } while (valg < 1 || valg > 5);
 
         String tekst = switch (valg) {
-            case 1 -> "Omg";
+            case 1 -> "Skriv inn ansatt ID";
             case 2 -> "Omg!";
             case 3 -> "Omg!!";
             case 4 -> "Omg!!!";
@@ -85,14 +77,43 @@ public class Main {
         };
         System.out.println(tekst);
 
+        int id;
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Skriv ett heltall!");
+        }
+        id = scanner.nextInt();
+
         // metodekall her
         switch (valg) {
-            case 1 -> System.out.println("todo");
+            case 1 -> sokEtterAnsatt(ansattDAO, id);
             case 2 -> System.out.println("todo");
             case 3 -> System.out.println("todo");
             case 4 -> System.out.println("todo");
             case 5 -> System.out.println("todo");
             default -> System.out.println("todo");
+        }
+        System.out.println();
+        System.out.println("Trykk ein tast for å avslutta");
+        scanner.nextLine();
+        scanner.nextLine(); // dette er ikke en feil
+    }
+
+    private static void sokEtterAnsatt(AnsattDAO ansattDAO, int id) {
+        try {
+            ansattDAO = new AnsattDAO();
+            int ansattId = id;
+            Ansatt ansatt = ansattDAO.finnAnsattMedId(ansattId);
+
+            if (ansatt != null) {
+                System.out.println("Found ansatt: " + ansatt.getFornavn() + " " + ansatt.getEtternavn());
+            } else {
+                System.out.println("Ansatt with ID " + ansattId + " not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ansattDAO.close();
         }
     }
 
