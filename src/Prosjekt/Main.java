0c8b2ba373b396156dffc33620abc3ff3de9c0ae
h581296 <581296@stud.hvl.no>
2024-04-03 20:24:41 +0200

    flyttet totale timer ut av loopen så det ikke blir printet flere ganger i 13

--- src/Prosjekt/Main.java
+++ src/Prosjekt/Main.java
@@ -180,8 +180,8 @@ public class Main {
                     sumTimer += p.getTimer();
                     System.out.println(p.getAnsatt() + ".");
                     System.out.println(p.getP_rolle() + ".");
-                    System.out.println("Totalt antall timer: " + sumTimer + ".");
                 }
+                System.out.println("Totalt antall timer: " + sumTimer + ".");
             }
 
             default -> {
